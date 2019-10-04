d988db7d388e9ba9f58e73e9e388c0c5e8b22386
Jon Barclay <my@nameisjon.com>
2019-10-04 13:56:08 -0500

    wrote js functions to add and delete rows from table, 3rd function to display total rows

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,34 @@
 // console.log('js');
 // console.log('jquery', $);
 
+$(readyNow);
+
+let meetupId = 0;
+let meetupCount = 0;
+
+function addMeetup() {
+    $('#meetupsTableBody').append(`<tr id="row${meetupId}"><td>${$('#newGroupInput').val()}</td><td>${$('#dateInput').val()}</td><td><button class="deleteButtons" id="deleteButton${meetupId}">Delete</button></td></tr>`);
+    meetupId++;
+    meetupCount++;
+    console.log(`In addMeetup: meetupId = ${meetupId} meetupCount = ${meetupCount}`);
+    domCount();
+}
+
+function removeMeetup(){
+    console.log('delete click');
+    $(this).closest('tr').remove();
+    meetupCount--;
+    domCount();
+}
+
+function domCount(){
+    $('#counter').text(`Total Meetups: ${meetupCount}`);
+}
+
+function readyNow() {
+    //listen for Add Meetup button press
+    $('#addMeetupButton').click(addMeetup);
+    //listen for Delete button press
+    $('tbody').on('click', '.deleteButtons', removeMeetup);
+    domCount();
+}
